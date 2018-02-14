be983dc2eda98af13d34888c058631ab669c86cc
nicolas_dell <nicolas.csanchez@gmail.com>
2018-02-14 16:49:01 +0100

    compute gat matrix, cluster corr and uncorr, plot gat matrix for each comp

--- scripts/on_tux/run_plot_gat.m
+++ scripts/on_tux/run_plot_gat.m
@@ -3,46 +3,102 @@
 
 % TODO: ALL GRAPHS
 % separate graphs; plot reference lines to each graphs (either on stim or cue)
+%% EXPECTATION
+%% COMPUTE THE GAT MATRIX EXPECTATION
+cfg                  = [];                                                           % clear the config variable
+cfg.startdir         = '/media/nicolas/Midgard/EEG_uva_fhr/Predictive_EEG/RESULTS/MVPA_RESULTS/MVPA_RESULTS_EXP';  % path to first level results 
+cfg.mpcompcor_method = 'cluster_based';                                              % multiple comparison correction method ('uncorrected' for uncorrected ploting)
+cfg.iterations       = 250;                                                          % reduce the number of iterations to save time
+mvpa_eeg_stats_exp   = adam_compute_group_MVPA(cfg);                                 % select RAW_EEG when dialog pops up
 
-%% COMPUTE THE GAT MATRIX OF ALL EEG AND MEG COMPARISONS
+%% PLOT THE GAT MATRIX EXPECTATION
+%% CUE prediction
 cfg = [];                                    % clear the config variable
-cfg.startdir = '/home/nicolas1/Documents/predictive_eeg/MVPA_RESULTS_EXP/';  % path to first level results 
-cfg.mpcompcor_method = 'cluster_based';      % multiple comparison correction method
-cfg.iterations = 250;                        % reduce the number of iterations to save time
-mvpa_eeg_stats_exp = adam_compute_group_MVPA(cfg);  % select RAW_EEG when dialog pops up
-
-%% PLOT THE GAT MATRIX OF ALL EEG AND MEG COMPARISONs
+cfg.referenceline = -2000;                   % ver/hor reference lines
+cfg.plot_order    = {'PRED_CUE'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_exp]);  % actual plotting, combine EEG/MEG results
+%% non-predicted stimuli
 cfg = [];                                    % clear the config variable
-cfg.plot_order = {'PRED_CUE' 'NONPRED_STIM' 'PRED_STIM'};
+cfg.referenceline = 0;                       % ver/hor reference lines
+cfg.plot_order    = {'NONPRED_STIM'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_exp]);  % actual plotting, combine EEG/MEG results
+%% predicted stimuli
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                       % ver/hor reference lines
+cfg.plot_order    = {'PRED_STIM'};
 adam_plot_MVPA(cfg, [mvpa_eeg_stats_exp]);  % actual plotting, combine EEG/MEG results
 
-
-%% UNCORRECTED
-
-%% COMPUTE THE GAT MATRIX OF ALL EEG AND MEG COMPARISONS
+%% UNCORRECTED expected comparisons
+%% COMPUTE THE GAT MATRIX EXPECTATION
 cfg = [];                                    % clear the config variable
-cfg.startdir = '/home/nicolas1/Documents/predictive_eeg/MVPA_RESULTS_EXP/';  % path to first level results 
+cfg.startdir         = '/media/nicolas/Midgard/EEG_uva_fhr/Predictive_EEG/RESULTS/MVPA_RESULTS/MVPA_RESULTS_EXP';  % path to first level results 
 cfg.mpcompcor_method = 'uncorrected';      % multiple comparison correction method
-cfg.iterations = 250;                        % reduce the number of iterations to save time
-mvpa_eeg_stats_UNCORRECTED = adam_compute_group_MVPA(cfg);  % select RAW_EEG when dialog pops up
+cfg.iterations       = 250;                        % reduce the number of iterations to save time
+mvpa_eeg_stats_exp_UNCORRECTED = adam_compute_group_MVPA(cfg);  % select RAW_EEG when dialog pops up
 
-%% PLOT THE GAT MATRIX OF ALL EEG AND MEG COMPARISONs
+%% PLOT THE GAT MATRIX EXPECTATION
 cfg = [];                                    % clear the config variable
-cfg.plot_order = {'PRED_CUE' 'NONPRED_STIM' 'PRED_STIM'};
-cfg.referenceline = -2000;
-adam_plot_MVPA(cfg, [mvpa_eeg_stats_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
+cfg.referenceline = -2000;                   % ver/hor reference lines
+cfg.plot_order    = {'PRED_CUE'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_exp_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
+%% PLOT THE GAT MATRIX EXPECTATION
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                   % ver/hor reference lines
+cfg.plot_order    = {'NONPRED_STIM'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_exp_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
+%% PLOT THE GAT MATRIX EXPECTATION
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                   % ver/hor reference lines
+cfg.plot_order    = {'PRED_STIM'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_exp_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
 
-%%
-%% COMPUTE THE GAT MATRIX OF ALL EEG AND MEG COMPARISONS
-cfg = [];                                    % clear the config variable
-cfg.startdir = '/home/nicolas1/Documents/predictive_eeg/MVPA_RESULTS_REL';  % path to first level results 
-cfg.mpcompcor_method = 'cluster_based';      % multiple comparison correction method
-cfg.iterations = 250;                        % reduce the number of iterations to save time
-mvpa_eeg_stats_rel = adam_compute_group_MVPA(cfg);  % select RAW_EEG when dialog pops up
+%% TASK RELEVANCE
+%% COMPUTE THE GAT MATRIX TASK RELEVANCE
+cfg                  = [];                                                           % clear the config variable
+cfg.startdir         = '/media/nicolas/Midgard/EEG_uva_fhr/Predictive_EEG/RESULTS/MVPA_RESULTS/MVPA_RESULTS_REL';  % path to first level results 
+cfg.mpcompcor_method = 'cluster_based';                                              % multiple comparison correction method ('uncorrected' for uncorrected ploting)
+cfg.iterations       = 250;                                                          % reduce the number of iterations to save time
+mvpa_eeg_stats_rel   = adam_compute_group_MVPA(cfg);                                 % select RAW_EEG when dialog pops up
 
-%% PLOT THE GAT MATRIX OF ALL EEG AND MEG COMPARISONs
+%% PLOT THE GAT MATRIX TASK RELEVANCE
+%% CUE prediction
 cfg = [];                                    % clear the config variable
-cfg.plot_order = {'PRED_CUE' 'NONPRED_STIM' 'PRED_STIM'};
+cfg.referenceline = -2000;                   % ver/hor reference lines
+cfg.plot_order    = {'PRED_CUE'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_rel]);  % actual plotting, combine EEG/MEG results
+%% non-predicted stimuli
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                       % ver/hor reference lines
+cfg.plot_order    = {'CATINCORR_STIM'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_rel]);  % actual plotting, combine EEG/MEG results
+%% predicted stimuli
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                       % ver/hor reference lines
+cfg.plot_order    = {'CATCORR_STIM'};
 adam_plot_MVPA(cfg, [mvpa_eeg_stats_rel]);  % actual plotting, combine EEG/MEG results
 
+%% UNCORRECTED TASK RELEVANCE comparisons
+%% COMPUTE THE GAT MATRIX TASK RELEVANCE
+cfg                  = [];                                                           % clear the config variable
+cfg.startdir         = '/media/nicolas/Midgard/EEG_uva_fhr/Predictive_EEG/RESULTS/MVPA_RESULTS/MVPA_RESULTS_REL';  % path to first level results 
+cfg.mpcompcor_method = 'uncorrected';                                              % multiple comparison correction method ('uncorrected' for uncorrected ploting)
+cfg.iterations       = 250;                                                          % reduce the number of iterations to save time
+mvpa_eeg_stats_rel_UNCORRECTED   = adam_compute_group_MVPA(cfg);                                 % select RAW_EEG when dialog pops up
+
+%% PLOT THE GAT MATRIX TASK RELEVANCE
+%% CUE prediction
+cfg = [];                                    % clear the config variable
+cfg.referenceline = -2000;                   % ver/hor reference lines
+cfg.plot_order    = {'PRED_CUE'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_rel_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
+%% non-predicted stimuli
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                       % ver/hor reference lines
+cfg.plot_order    = {'CATINCORR_STIM'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_rel_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
+%% predicted stimuli
+cfg = [];                                    % clear the config variable
+cfg.referenceline = 0;                       % ver/hor reference lines
+cfg.plot_order    = {'CATCORR_STIM'};
+adam_plot_MVPA(cfg, [mvpa_eeg_stats_rel_UNCORRECTED]);  % actual plotting, combine EEG/MEG results
 
